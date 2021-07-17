ad2bba4efd8fe5f07ba4b168e35e793493762457
U-ki-M <u.ki.murayama@gmail.com>
2021-07-17 17:48:39 +0900

    エラー文表示と項目の順番を一致させた

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,12 @@ class Item < ApplicationRecord
   validates :image,        presence: true
   validates :title,        presence: true
   validates :description,  presence: true
-  validates :price,        presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :category_id,  numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :fee_by_id,    numericality: { other_than: 1 , message: "can't be blank"}
   validates :from_id,      numericality: { other_than: 1 , message: "can't be blank"}
   validates :timeline_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,        presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
