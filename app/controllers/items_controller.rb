3cb5662af0206ff71612a0b9f405a8a7797d2c18
U-ki-M <u.ki.murayama@gmail.com>
2021-07-19 12:36:14 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -11,15 +12,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to root_path
@@ -48,4 +46,9 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == item.user_id
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
