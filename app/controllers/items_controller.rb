a85560cdd4710be43c5a068d3ab205e06172b604
U-ki-M <u.ki.murayama@gmail.com>
2021-07-19 11:49:06 +0900

    編集画面の実装とエラーハンドリングメッセージ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
     if item.save
       redirect_to root_path
     else
-      @item = Item.find(params[:id])
+      @item = item
       render :edit
     end
   end
